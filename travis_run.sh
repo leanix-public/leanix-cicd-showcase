8dbf4a2ff0ae3457394ab1f72d7a0bf55d2eb881
dominikrose-leanix <dominik.rose@leanix.net>
2021-03-31 20:45:52 +0200

    Update travis_run.sh

--- travis_run.sh
+++ travis_run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+AUTH_URL=https://${LEANIX_HOST}/services/mtm/v1/oauth2/token
+URL=https://${LEANIX_HOST}/services/cicd-connector/v2/deployment
+
 FILE="`pwd`/metadata.yaml"
-BEARER=$(curl --url https://${LEANIX_HOST}/services/mtm/v1/oauth2/token -u apitoken:${LEANIX_TOKEN} --data grant_type=client_credentials | jq -r '.access_token') 
+BEARER=$(curl ${AUTH_URL} -u apitoken:${LEANIX_TOKEN} --data grant_type=client_credentials | jq -r '.access_token') 
 echo $(curl -H "Authorization: Bearer $BEARER" -H 'Content-Type: multipart/form-data' -F manifest="@$FILE" -F 'data={"version": "1.0.0", "stage": "dev"}' $URL)
