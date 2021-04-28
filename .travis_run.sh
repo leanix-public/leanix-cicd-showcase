a2b13880f88b63c58e8ea4ec95b44996a809f138
Dominik Rose <dominik.rose@leanix.net>
2021-04-28 14:14:29 +0200

    Update

--- .travis_run.sh
+++ .travis_run.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-AUTH_URL=https://${LEANIX_HOST}/services/mtm/v1/oauth2/token
-URL=https://${LEANIX_HOST}/services/cicd-connector/v2/deployment
+### Extract the npm dependencies
+license-checker --json > dependencies.json 
 
-FILE="`pwd`/metadata.yaml"
+### Get a bearer token
+AUTH_URL=https://${LEANIX_HOST}/services/mtm/v1/oauth2/token
 BEARER=$(curl ${AUTH_URL} -u apitoken:${LEANIX_TOKEN} --data grant_type=client_credentials | jq -r '.access_token') 
-echo $(curl -H "Authorization: Bearer $BEARER" -H 'Content-Type: multipart/form-data' -F manifest="@$FILE" -F 'data={"version": "1.0.0", "stage": "dev"}' $URL)
+
+### Call the CICD connector
+URL=https://${LEANIX_HOST}/services/cicd-connector/v2/deployment
+MANIFEST="`pwd`/metadata.yaml"
+DEPENDENCIES="`pwd`/dependencies.json"
+
+curl -X POST \
+  -H "Authorization: Bearer $BEARER"\
+  -H "Content-Type: multipart/form-data" \
+  -F manifest="@$MANIFEST" \
+  -F dependencies="@$DEPENDENCIES" \
+  -F 'data={
+  "version": "1.0.1",
+  "stage": "dev",
+  "dependencyManager": "NPM"
+}' $URL
+echo $?
